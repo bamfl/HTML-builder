b5581c948af0f9661aa289134b727dc41324e03a
Dmitriy Petukhov <pdnsite@gmail.com>
2021-11-04 16:59:09 +0300

    feat: add 02 write file task solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,5 +1,4 @@
-const process = require('process');
-const { stdin: input, stdout: output } = process;
+const { stdin: input, stdout: output } = require('process');
 
 const readline = require('readline');
 const rl = readline.createInterface({ input, output });
@@ -7,25 +6,34 @@ const rl = readline.createInterface({ input, output });
 const path = require('path');
 const fs = require('fs');
 
-const filePath = path.join(__dirname, 'write-text.txt');
-const readStream = fs.createReadStream(filePath);
-const writeStream = fs.createWriteStream(filePath);
+const filePath = path.join(__dirname, 'text.txt');
+let writeStream = fs.createWriteStream(filePath);
 
 rl.on('SIGINT', () => {
-  rl.question('Are you sure you want to exit? ', (answer) => {
-    if (answer.match(/^y(es)?$/i)) {
-      rl.close();
-    }
-  });
+  console.log('Exit from process');
+  rl.close();
 });
 
 rl.on('line', (input) => {
-  readStream.on('data', (chunk) => {
-    writeStream.write(chunk);
-    writeStream.write(input);
-  });
+  if (input === 'exit') {
+    console.log('Exit from process');
+    rl.close();
+  } else {
+    fs.readFile(filePath, 'UTF-8', (error, text) => {
+      writeStream = fs.createWriteStream(filePath);
+      writeStream.write(`${text}${input}`);
+    });
+  }
 });
 
-rl.question('Please, enter the text: \n', (answer1) => {
-  writeStream.write(`${answer1} \n`);
+rl.question('Please, enter the text: \n', (answer) => {
+  if (answer === 'exit') {
+    console.log('Exit from process');
+    rl.close();
+  } else {
+    fs.readFile(filePath, 'UTF-8', (error, text) => {
+      writeStream = fs.createWriteStream(filePath);
+      writeStream.write(`${text}${answer}`);
+    });
+  }
 });
