1c48009e4e151e390964ffbb2e6f4d007a94ecd4
Dmitriy Petukhov <pdnsite@gmail.com>
2021-11-05 17:52:17 +0300

    feat: add 05 merge styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { rm, writeFile, readFile, appendFile, readdir } = require('fs/promises');
+
+const sourceFolderPath = path.join(__dirname, 'styles');
+const distFolderPath = path.join(__dirname, 'project-dist');
+const bundleFilePath = path.join(distFolderPath, 'bundle.css');
+
+writeFile(bundleFilePath, '');
+
+const createBundle = async () => {
+  await rm(bundleFilePath, { recursive: true });
+
+  const files = await readdir(sourceFolderPath, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isFile(file)) {
+      const filePath = path.join(sourceFolderPath, file.name);
+      const fileExt = path.parse(filePath).ext;
+
+      if (fileExt === '.css') {
+        const data = await readFile(filePath, 'utf-8');
+
+        await appendFile(bundleFilePath, `${data}\n\n`);
+      }
+    }
+  }
+};
+
+createBundle();
