adbcc8fbd3dd6756020a0f24251a5ca531c9f5fe
Dmitriy Petukhov <pdnsite@gmail.com>
2021-11-05 12:37:18 +0300

    feat: add 04 copy directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
 
+const sourceFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
+
+mkdir(copyFolderPath, { recursive: true });
+
+const copyDir = async () => {
+  await rm(copyFolderPath, { recursive: true });
+
+  await mkdir(copyFolderPath, { recursive: true });
+
+  const files = await readdir(sourceFolderPath, { withFileTypes: true });
+
+  for (const file of files) {
+    const filePath = path.join(sourceFolderPath, file.name);
+    const copyFilePath = path.join(copyFolderPath, file.name);
+
+    await copyFile(filePath, copyFilePath);
+  }
+};
+
+copyDir();
